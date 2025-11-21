ff279b831d6cab3963044e703da6ffaf36c881b3
Mert Bicak <mbicak@redhat.com>
2025-11-21 14:04:12 +0100

    feat: add initial folder structure

---
+++ src/main/java/org/jboss/sbomer/syft/generator/core/port/spi/FailureNotifier.java
@@ -0,0 +1,13 @@
+package main.java.org.jboss.sbomer.syft.generator.core.port.spi;
+
+import org.jboss.sbomer.events.common.FailureSpec;
+
+public interface FailureNotifier {
+    /**
+     * Notifies an external system about a failure.
+     * @param failure The standardized details of the failure.
+     @param correlationId The correlationId associated with failing event
+      * @param sourceEvent The original event object that caused the failure.
+     */
+    void notify(FailureSpec failure, String correlationId, Object sourceEvent);
+}

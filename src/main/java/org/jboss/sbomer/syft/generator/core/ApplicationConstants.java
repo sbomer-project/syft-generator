126f416fb278c1d413d3cdf4618f5bb0d77afac7
Mert Bicak <mbicak@redhat.com>
2025-11-21 14:04:12 +0100

    feat: add initial folder structure

---
+++ src/main/java/org/jboss/sbomer/syft/generator/core/ApplicationConstants.java
@@ -0,0 +1,9 @@
+package main.java.org.jboss.sbomer.syft.generator.core;
+
+public class ApplicationConstants {
+
+    private ApplicationConstants() {}
+
+    public static final String COMPONENT_NAME = "syft-generator";
+
+}
